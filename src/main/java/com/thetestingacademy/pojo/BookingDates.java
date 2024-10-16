fc8cd6a3583e660ebddc50624c73207668362d41
pranathi <pranathikatta26@gmail.com>
2024-10-15 20:48:34 -0400

    Added Basic Structure

---
+++ src/main/java/com/thetestingacademy/pojo/BookingDates.java
@@ -0,0 +1,23 @@
+package com.thetestingacademy.pojo;
+
+//POJO classes - Plain object Java classes
+public class BookingDates {
+    String checkin;
+    String checkout;
+
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+}
